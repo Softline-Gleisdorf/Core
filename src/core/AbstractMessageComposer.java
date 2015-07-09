944818d7ee3695491068160141510175d059ae4b
Georg Mansky-Kummert <georg@softline.at>
2015-07-09 13:05:35 +0200

    "Noch eine kleine Änderung..."

--- src/core/AbstractMessageComposer.java
+++ src/core/AbstractMessageComposer.java
@@ -4,7 +4,7 @@ public abstract class AbstractMessageComposer implements MessageComposer {
 
 	@Override
 	public String composeMessage() {
-		return "Kleiner Änderung der Nachricht von " + getClass().getSimpleName();
+		return "Nachricht von " + getClass().getSimpleName();
 	}
 
 }
