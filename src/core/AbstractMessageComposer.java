267bc445f44109d402fbc35a026295656a125248
Georg Mansky-Kummert <georg@softline.at>
2015-07-09 08:57:29 +0200

    "La la so so."

--- src/core/AbstractMessageComposer.java
+++ src/core/AbstractMessageComposer.java
@@ -4,7 +4,7 @@ public abstract class AbstractMessageComposer implements MessageComposer {
 
 	@Override
 	public String composeMessage() {
-		return "Nachricht von " + getClass().getSimpleName() + " (Corecodeänderung)";
+		return "Nachricht von " + getClass().getSimpleName();
 	}
 
 }
